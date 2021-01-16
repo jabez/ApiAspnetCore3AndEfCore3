a8334597d8476de881ddf1e988ef8ae8e3aff6c0
jabez <jabez_t.i@hotmail.com>
2021-01-16 14:50:52 -0300

    feat: Desenvolvimento da estrutura inicial do projeto. feat: Desenvolvimento dos controllers de produto e categoria.

---
+++ Shop/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x => x.Category).AsNoTracking().ToListAsync();
+            return products;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetById(int id, [FromServices] DataContext context)
+        {
+            var products = await context
+                                 .Products
+                                 .Include(x => x.Category)
+                                 .AsNoTracking()
+                                 .FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory(int id, [FromServices] DataContext context)
+        {
+            var products = await context
+                                 .Products
+                                 .Include(x => x.Category)
+                                 .AsNoTracking()
+                                 .Where(x => x.Category.Id == id)
+                                 .ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post([FromServices] DataContext context, [FromBody] Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+    }
+}
